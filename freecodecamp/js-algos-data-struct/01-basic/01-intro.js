bfb07113b94360ddc9eb531a9b92625a37d5a575
Peter Kim <petekim@gmail.com>
2020-10-07 10:55:54 -0700

    Create 01-intro.js

---
+++ freecodecamp/js-algos-data-struct/01-basic/01-intro.js
@@ -0,0 +1,53 @@
+// Here is an in-line comment
+
+/* 
+Here is a 
+multi-line 
+comment
+*/
+
+// Declare JavaScript Variables
+var myName;
+
+// Storing values in variables
+var a;
+a = 7;
+
+// Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+// Initializing Variables with the Assignment Operator
+var a = 9;
+
+// Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Add Two Numbers with JavaScript
+var sum = 10 + 10;
+
+// Subtract One Number from Another with JavaScript
+var difference = 45 - 33;
+
+// Multiply Two Numbers with JavaScript
+var product = 8 * 10;
+
+// Divide One Number by Another with JavaScript
+var quotient = 66 / 33;
+
+// Increment a Number with JavaScript
