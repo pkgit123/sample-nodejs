ef6d1ab8743f75329fa54c5567edf347de4731ff
Peter Kim <petekim@gmail.com>
2020-10-08 22:54:29 -0700

    Completed 54 out of 111 in 01-basic-javascript

--- freecodecamp/js-algos-data-struct/01-basic-javascript.js
+++ freecodecamp/js-algos-data-struct/01-basic-javascript.js
@@ -275,6 +275,97 @@ console.log('outside myLocalScope', myVar);
 
 
 /* ==========================================
+    Global vs. Local Scope in Functions
+   ==========================================
+*/
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Return a Value from a Function with Return
+   ==========================================
+*/
+
+function timesFive(num) {
+    return num * 5;
+}
+timesFive(5);
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Understanding Undefined Value returned from a Function
+   ==========================================
+*/
+
+// Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Assignment with a Returned Value
+   ==========================================
+*/
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+/* ==========================================
     lessonName
    ==========================================
 */
@@ -292,13 +383,6 @@ console.log('outside myLocalScope', myVar);
 */
 
 
-/* ==========================================
-    lessonName
-   ==========================================
-*/
-
-/* ==========================================
-*/
 
 
 /* ==========================================
@@ -321,7 +405,22 @@ console.log('outside myLocalScope', myVar);
 
 
 
+/* ==========================================
+    lessonName
+   ==========================================
+*/
 
+/* ==========================================
+*/
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
 
 
 
