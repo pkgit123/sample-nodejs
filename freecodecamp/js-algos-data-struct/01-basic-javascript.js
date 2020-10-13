a36f4e115f46a89fe27e131a9128f1b11b85cf15
Peter Kim <petekim@gmail.com>
2020-10-13 14:12:55 -0700

    Completed 70 out of 111 in 01-basic-javascript

--- freecodecamp/js-algos-data-struct/01-basic-javascript.js
+++ freecodecamp/js-algos-data-struct/01-basic-javascript.js
@@ -356,6 +356,391 @@ processed = processArg(7);
 
 
 /* ==========================================
+    Basic JavaScript: Stand in Line
+   ==========================================
+*/
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item)
+  return arr.shift();
+  // Only change code above this line
+  
+
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+/* ==========================================
+*/
+
+
+
+/* ==========================================
+    Basic JavaScript: Understanding Boolean Values
+   ==========================================
+*/
+function welcomeToBooleans() {
+
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Basic JavaScript: Use Conditional Logic with If Statements
+   ==========================================
+*/
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  // Only change code above this line
+
+}
+
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Equality Operator
+   ==========================================
+*/
+// Setup
+function testEqual(val) {
+  if (val==12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Strict Equality Operator
+   ==========================================
+*/
+
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Basic JavaScript: Practice comparing different values
+   ==========================================
+*/
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Inequality Operator
+   ==========================================
+*/
+// Setup
+function testNotEqual(val) {
+  if (val!=99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+/* ==========================================
+*/
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Strict Inequality Operator
+   ==========================================
+*/
+// Setup
+function testStrictNotEqual(val) {
+  if (val!==17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+/* ==========================================
+*/
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Greater Than Operator
+   ==========================================
+*/
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+/* ==========================================
+*/
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Greater Than Or Equal To Operator
+   ==========================================
+*/
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Less Than Operator
+   ==========================================
+*/
+function testLessThan(val) {
+  if (val<25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val<55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparison with the Less Than Or Equal To Operator
+   ==========================================
+*/
+function testLessOrEqual(val) {
+  if (val<=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparisons with the Logical And Operator
+   ==========================================
+*/
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >=25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Comparisons with the Logical Or Operator
+   ==========================================
+*/
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Introducing Else Statements
+   ==========================================
+*/
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Introducing Else If Statements
+   ==========================================
+*/
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Logical Order in If Else Statements
+   ==========================================
+*/
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    Basic JavaScript: Chaining If Else Statements
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
     lessonName
    ==========================================
 */
@@ -365,14 +750,6 @@ processed = processArg(7);
 
 
 
-/* ==========================================
-    lessonName
-   ==========================================
-*/
-
-/* ==========================================
-*/
-
 
 /* ==========================================
     lessonName
@@ -383,15 +760,6 @@ processed = processArg(7);
 */
 
 
-
-
-/* ==========================================
-    lessonName
-   ==========================================
-*/
-
-/* ==========================================
-*/
 
 
 /* ==========================================
@@ -414,6 +782,8 @@ processed = processArg(7);
 */
 
 
+
+
 /* ==========================================
     lessonName
    ==========================================
@@ -425,6 +795,101 @@ processed = processArg(7);
 
 
 
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
+
+
+
+
+/* ==========================================
+    lessonName
+   ==========================================
+*/
+
+/* ==========================================
+*/
 
 
 
