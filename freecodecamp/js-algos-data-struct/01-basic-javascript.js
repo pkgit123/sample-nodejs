f28aefa1d7db252b20213e45d25725b6a3ee44be
Peter Kim <petekim@gmail.com>
2020-10-21 11:20:28 -0700

    Completed 99 out of 111 in 01-basic-javascript

--- freecodecamp/js-algos-data-struct/01-basic-javascript.js
+++ freecodecamp/js-algos-data-struct/01-basic-javascript.js
@@ -1431,59 +1431,112 @@ while (i < 6) {
 
 
 /* ==========================================
-    lessonName
+    Basic JavaScript: Iterate with JavaScript For Loops
+   ==========================================
+*/
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+/* ==========================================
+*/
+
+
+
+/* ==========================================
+    Basic JavaScript: Iterate Odd Numbers With a For Loop
+   ==========================================
+*/
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i < 10; i+=2) {
+    myArray.push(i);
+}
+/* ==========================================
+*/
+
+
+
+/* ==========================================
+    Basic JavaScript: Count Backwards With a For Loop
    ==========================================
 */
 
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 9; i > 0; i -= 2) {
+    myArray.push(i);
+}
+
 /* ==========================================
 */
 
 
 
 /* ==========================================
-    lessonName
+    Basic JavaScript: Iterate Through an Array with a For Loop
    ==========================================
 */
 
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+}
+
 /* ==========================================
 */
 
 
 
 /* ==========================================
-    lessonName
+    Basic JavaScript: Nesting For Loops
    ==========================================
 */
 
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i=0; i < arr.length; i++) {
+    for (var j=0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
 /* ==========================================
 */
 
 
 
 /* ==========================================
-    lessonName
+    Basic JavaScript: Iterate with JavaScript Do...While Loops
    ==========================================
 */
 
-/* ==========================================
-*/
+// Setup
+var myArray = [];
+var i = 10;
 
-
-
-/* ==========================================
-    lessonName
-   ==========================================
-*/
-
-/* ==========================================
-*/
-
-
-
-/* ==========================================
-    lessonName
-   ==========================================
-*/
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 11);
 
 /* ==========================================
 */
