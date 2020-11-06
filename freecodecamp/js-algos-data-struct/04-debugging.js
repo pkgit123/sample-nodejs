476c5d314eaad7c27648c6305bcf7499f2e3bb4b
Peter Kim <petekim@gmail.com>
2020-11-06 10:41:50 -0800

    Create 04-debugging.js

---
+++ freecodecamp/js-algos-data-struct/04-debugging.js
@@ -0,0 +1,88 @@
+/**
+ * Filename:    04-debugging.js
+ * 
+ * Types of errors:
+ *  (1) syntax errors that prevent a program from running (e.g. typo)
+ *  (2) runtime errors when code fails to execute or has unexpected behavior (e.g. infinite loop)
+ *  (3) semantic (or logical) errors when code doesn't do what it's meant to (e.g. adding numbers instead of multiplying)
+ */
+ 
+/** =================================
+ *  Debugging: Use the JavaScript Console to Check the Value of a Variable
+ *  JavaScript console called DevTools
+ *  console.log()
+ */
+
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a)
+console.log(b)
+
+let sumAB = a + b;
+console.log(sumAB);
+
+
+/** =================================
+ *  Debugging: Understanding the Differences between the freeCodeCamp and Browser Console
+ */
+
+
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
+/** =================================
+ * 
+ */
+
+
