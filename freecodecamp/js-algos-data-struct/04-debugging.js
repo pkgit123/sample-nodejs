abb72a706f20a523d354f44862e76eda407045ff
Peter Kim <petekim@gmail.com>
2020-11-10 22:32:35 -0800

    Completed all 12 in 04-debugging.js

--- freecodecamp/js-algos-data-struct/04-debugging.js
+++ freecodecamp/js-algos-data-struct/04-debugging.js
@@ -121,25 +121,69 @@ console.log(result);
  *  Debugging: Catch Arguments Passed in the Wrong Order When Calling a Function
  */
 
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
 
 
 /** =================================
- * 
+ *  Debugging: Catch Off By One Errors When Using Indexing
  */
+
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i < len; i++) {
+  // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
 
 
 /** =================================
- * 
+ *  Debugging: Use Caution When Reinitializing Variables Inside a Loop
+ *  Solution requires reinitializing row array in outer loop.
  */
+
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+    // Reinitialize row to empty array
+    row = [];
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
 
 
 /** =================================
- * 
+ *  Debugging: Prevent Infinite Loops with a Valid Terminal Condition
  */
 
-
-/** =================================
- * 
- */
-
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
 
