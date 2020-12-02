38d636429d9b18a094460c851b45c39627de7dab
Peter Kim <petekim@gmail.com>
2020-12-01 21:57:56 -0800

    Completed 16 out of 26 in 07-object-oriented.js

--- freecodecamp/js-algos-data-struct/07-object-oriented.js
+++ freecodecamp/js-algos-data-struct/07-object-oriented.js
@@ -200,39 +200,127 @@ function joinDogFraternity(candidate) {
 
 /** =============================================
  *  Object Oriented Programming: Change the Prototype to a New Object
- *  
+ *  Javascript object has key-value format, where key is method and value is function()
  */
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+  numLegs: 4,
+  eat: function() {
+    console.log('bite bite bite');
+  },
+  describe: function() {
+    console.log('My name is ' + this.name);
+  }
+
+};
 
 
 
 /** =============================================
- *  
+ *  Object Oriented Programming: Remember to Set the Constructor Property when Changing the Prototype
  *  
  */
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
 
 
 /** =============================================
- *  
+ *  Object Oriented Programming: Understand Where an Object’s Prototype Comes From
  *  
  */
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Only change code below this line
+console.log(Dog.prototype.isPrototypeOf(beagle));
+
+/** =============================================
+ *  Object Oriented Programming: Understand the Prototype Chain
+ *  
+ */
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
 
 
 /** =============================================
- *  
+ *  Object Oriented Programming: Use Inheritance So You Don't Repeat Yourself
  *  
  */
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
 
 
 /** =============================================
- *  
+ *  Object Oriented Programming: Inherit Behaviors from a Supertype
  *  
  */
 
+function Animal() { }
 
-/** =============================================
- *  
- *  
- */
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
 
 
 /** =============================================
