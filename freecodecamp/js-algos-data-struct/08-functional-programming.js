39eee92d46e8728a0bcfdc795cddfed34b84586d
Peter Kim <petekim@gmail.com>
2020-12-14 23:03:07 -0800

    Complete 4 out of 24 in 08-functional-programming.js

--- freecodecamp/js-algos-data-struct/08-functional-programming.js
+++ freecodecamp/js-algos-data-struct/08-functional-programming.js
@@ -66,15 +66,78 @@ console.log(
 
 
 
-/ ** 
-  *   
+/ ** Functional Programming: Understand the Hazards of Using Imperative Code
+  *  In English (and many other languages), the imperative tense is used to give commands. 
+  *  Similarly, an imperative style in programming is one that gives the computer a set of statements to perform a task. 
+  *  In contrast, functional programming is a form of declarative programming. You tell the computer what you want done by calling a method or function.
+  *  The .splice() method adds/removes items in an array.  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+  *  The .slice() method returns copy of portion of array.  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
   */
   
+// tabs is an array of titles of each site open within the window
+var Window = function(tabs) {
+  this.tabs = tabs; // We keep a record of the array inside the object
+};
 
-/ ** 
-  *   
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab'); // Let's open a new tab for now
+  return this;
+};
+
+// When you close a tab
+Window.prototype.tabClose = function (index) {
+
+  // Only change code below this line
+
+  var tabsBeforeIndex = this.tabs.slice(0, index); // Get the tabs before the tab
+  var tabsAfterIndex = this.tabs.slice(index + 1); // Get the tabs after the tab
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+
+  // Only change code above this line
+
+  return this;
+ };
+
+// Let's create three browser windows
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+var finalTabs = socialWindow
+  .tabOpen() // Open a new tab for cat memes
+  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+
+
+/ ** Functional Programming: Avoid Mutations and Side Effects Using Functional Programming
+  *  One of the core principles of functional programming is to not change things. 
+  *  Recall that in functional programming, changing or altering things is called mutation, and the outcome is called a side effect. 
+  *  A function, ideally, should be a pure function, meaning that it does not cause any side effects.
   */
   
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+
+  return fixedValue + 1;
+
+  // Only change code above this line
+}
+
+console.log(incrementer()); 
+
 
 / ** 
   *   
